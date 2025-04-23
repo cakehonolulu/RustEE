6dd76493a6a4bbc85c0c3704b2358c0726911c9f
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2025-04-23 23:19:49 +0200

    rustee: Configurable Bus emulation mode

--- rustee/src/main.rs
+++ rustee/src/main.rs
@@ -27,6 +27,10 @@ fn main() {
             .value_parser(["interpreter", "jit"])
             .default_value("jit")
             .help("Choose the EE backend: 'interpreter' or 'jit'"))
+        .arg(arg!(--"bus-mode" <MODE>)
+            .value_parser(["ranged", "sw_fastmem"])
+            .default_value("ranged")
+            .help("Choose the bus emulation mode: 'ranged' or 'sw_fastmem'"))
         .get_matches();
 
         tracing_subscriber::fmt()
@@ -42,7 +46,13 @@ fn main() {
     if let Some(bios_path) = arguments.get_one::<String>("bios") {
         let bios_path = Path::new(bios_path);
         let bios: BIOS = BIOS::new(bios_path).expect("Failed to load BIOS");
-        let bus = Bus::new(BusMode::Ranged, bios);
+
+        let bus_mode = match arguments.get_one::<String>("bus-mode").map(String::as_str) {
+            Some("sw_fastmem") => BusMode::SoftwareFastMem,
+            _ => BusMode::Ranged,
+        };
+
+        let bus = Bus::new(bus_mode, bios);
         let mut ee: EE = EE::new(bus);
 
         if let Some(breakpoints) = arguments.get_one::<Vec<u32>>("ee-breakpoint") {
