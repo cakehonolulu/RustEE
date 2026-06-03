de5069bbdbc2ca7613efea94240241264c50b29f
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2026-06-03 10:14:51 +0200

    bus/ranged: Remove all unsafe unaligned accesses with slice usage
    Signed-off-by: cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>

--- librustee/src/bus/ranged.rs
+++ librustee/src/bus/ranged.rs
@@ -72,8 +72,8 @@ pub fn init_ranged_tlb_mappings(bus: &mut Bus) {
 impl Bus {
     pub fn ranged_read8(&mut self, va: u32) -> u8 {
         if let Some(offset) = map::SCRATCHPAD.contains(va) {
-            let ptr = unsafe { self.scratchpad.as_ptr().add(offset as usize) } as *const u8;
-            return unsafe { ptr.read_unaligned() };
+            let o = offset as usize;
+            return u8::from_le_bytes(self.scratchpad[o..o + 1].try_into().unwrap());
         }
 
         let cop0_asid = { self.read_cop0_asid() };
@@ -86,14 +86,14 @@ impl Bus {
         };
 
         if let Some(offset) = map::RAM.contains(pa) {
-            let ptr = unsafe { self.ram.as_ptr().add(offset as usize) } as *const u8;
-            unsafe { ptr.read_unaligned() }
+            let o = offset as usize;
+            u8::from_le_bytes(self.ram[o..o + 1].try_into().unwrap())
         } else if let Some(offset) = map::SCRATCHPAD.contains(pa) {
-            let ptr = unsafe { self.scratchpad.as_ptr().add(offset as usize) } as *const u8;
-            unsafe { ptr.read_unaligned() }
+            let o = offset as usize;
+            u8::from_le_bytes(self.scratchpad[o..o + 1].try_into().unwrap())
         } else if let Some(offset) = map::BIOS.contains(pa) {
-            let ptr = unsafe { self.bios.bytes.as_ptr().add(offset as usize) } as *const u8;
-            unsafe { ptr.read_unaligned() }
+            let o = offset as usize;
+            u8::from_le_bytes(self.bios.bytes[o..o + 1].try_into().unwrap())
         } else {
             self.io_read8(pa)
         }
@@ -101,8 +101,8 @@ impl Bus {
 
     pub fn ranged_read16(&mut self, va: u32) -> u16 {
         if let Some(offset) = map::SCRATCHPAD.contains(va) {
-            let ptr = unsafe { self.scratchpad.as_ptr().add(offset as usize) } as *const u16;
-            return unsafe { ptr.read_unaligned() };
+            let o = offset as usize;
+            return u16::from_le_bytes(self.scratchpad[o..o + 2].try_into().unwrap());
         }
 
         let cop0_asid = { self.read_cop0_asid() };
@@ -115,14 +115,14 @@ impl Bus {
         };
 
         if let Some(offset) = map::RAM.contains(pa) {
-            let ptr = unsafe { self.ram.as_ptr().add(offset as usize) } as *const u16;
-            unsafe { ptr.read_unaligned() }
+            let o = offset as usize;
+            u16::from_le_bytes(self.ram[o..o + 2].try_into().unwrap())
         } else if let Some(offset) = map::SCRATCHPAD.contains(pa) {
-            let ptr = unsafe { self.scratchpad.as_ptr().add(offset as usize) } as *const u16;
-            unsafe { ptr.read_unaligned() }
+            let o = offset as usize;
+            u16::from_le_bytes(self.scratchpad[o..o + 2].try_into().unwrap())
         } else if let Some(offset) = map::BIOS.contains(pa) {
-            let ptr = unsafe { self.bios.bytes.as_ptr().add(offset as usize) } as *const u16;
-            unsafe { ptr.read_unaligned() }
+            let o = offset as usize;
+            u16::from_le_bytes(self.bios.bytes[o..o + 2].try_into().unwrap())
         } else {
             self.io_read16(pa)
         }
@@ -130,8 +130,8 @@ impl Bus {
 
     pub fn ranged_read32(&mut self, va: u32) -> u32 {
         if let Some(offset) = map::SCRATCHPAD.contains(va) {
-            let ptr = unsafe { self.scratchpad.as_ptr().add(offset as usize) } as *const u32;
-            return unsafe { ptr.read_unaligned() };
+            let o = offset as usize;
+            return u32::from_le_bytes(self.scratchpad[o..o + 4].try_into().unwrap());
         }
 
         let cop0_asid = { self.read_cop0_asid() };
@@ -144,14 +144,14 @@ impl Bus {
         };
 
         if let Some(offset) = map::RAM.contains(pa) {
-            let ptr = unsafe { self.ram.as_ptr().add(offset as usize) } as *const u32;
-            unsafe { ptr.read_unaligned() }
+            let o = offset as usize;
+            u32::from_le_bytes(self.ram[o..o + 4].try_into().unwrap())
         } else if let Some(offset) = map::SCRATCHPAD.contains(pa) {
-            let ptr = unsafe { self.scratchpad.as_ptr().add(offset as usize) } as *const u32;
-            unsafe { ptr.read_unaligned() }
+            let o = offset as usize;
+            u32::from_le_bytes(self.scratchpad[o..o + 4].try_into().unwrap())
         } else if let Some(offset) = map::BIOS.contains(pa) {
-            let ptr = unsafe { self.bios.bytes.as_ptr().add(offset as usize) } as *const u32;
-            unsafe { ptr.read_unaligned() }
+            let o = offset as usize;
+            u32::from_le_bytes(self.bios.bytes[o..o + 4].try_into().unwrap())
         } else {
             self.io_read32(pa)
         }
@@ -159,8 +159,8 @@ impl Bus {
 
     pub fn ranged_read64(&mut self, va: u32) -> u64 {
         if let Some(offset) = map::SCRATCHPAD.contains(va) {
-            let ptr = unsafe { self.scratchpad.as_ptr().add(offset as usize) } as *const u64;
-            return unsafe { ptr.read_unaligned() };
+            let o = offset as usize;
+            return u64::from_le_bytes(self.scratchpad[o..o + 8].try_into().unwrap());
         }
 
         let cop0_asid = { self.read_cop0_asid() };
@@ -173,14 +173,14 @@ impl Bus {
         };
 
         if let Some(offset) = map::RAM.contains(pa) {
-            let ptr = unsafe { self.ram.as_ptr().add(offset as usize) } as *const u64;
-            unsafe { ptr.read_unaligned() }
+            let o = offset as usize;
+            u64::from_le_bytes(self.ram[o..o + 8].try_into().unwrap())
         } else if let Some(offset) = map::SCRATCHPAD.contains(pa) {
-            let ptr = unsafe { self.scratchpad.as_ptr().add(offset as usize) } as *const u64;
-            unsafe { ptr.read_unaligned() }
+            let o = offset as usize;
+            u64::from_le_bytes(self.scratchpad[o..o + 8].try_into().unwrap())
         } else if let Some(offset) = map::BIOS.contains(pa) {
-            let ptr = unsafe { self.bios.bytes.as_ptr().add(offset as usize) } as *const u64;
-            unsafe { ptr.read_unaligned() }
+            let o = offset as usize;
+            u64::from_le_bytes(self.bios.bytes[o..o + 8].try_into().unwrap())
         } else {
             self.io_read64(pa)
         }
@@ -188,8 +188,8 @@ impl Bus {
 
     pub fn ranged_read128(&mut self, va: u32) -> u128 {
         if let Some(offset) = map::SCRATCHPAD.contains(va) {
-            let ptr = unsafe { self.scratchpad.as_ptr().add(offset as usize) } as *const u128;
-            return unsafe { ptr.read_unaligned() };
+            let o = offset as usize;
+            return u128::from_le_bytes(self.scratchpad[o..o + 16].try_into().unwrap());
         }
 
         let cop0_asid = { self.read_cop0_asid() };
@@ -202,14 +202,14 @@ impl Bus {
         };
 
         if let Some(offset) = map::RAM.contains(pa) {
-            let ptr = unsafe { self.ram.as_ptr().add(offset as usize) } as *const u128;
-            unsafe { ptr.read_unaligned() }
+            let o = offset as usize;
+            u128::from_le_bytes(self.ram[o..o + 16].try_into().unwrap())
         } else if let Some(offset) = map::SCRATCHPAD.contains(pa) {
-            let ptr = unsafe { self.scratchpad.as_ptr().add(offset as usize) } as *const u128;
-            unsafe { ptr.read_unaligned() }
+            let o = offset as usize;
+            u128::from_le_bytes(self.scratchpad[o..o + 16].try_into().unwrap())
         } else if let Some(offset) = map::BIOS.contains(pa) {
-            let ptr = unsafe { self.bios.bytes.as_ptr().add(offset as usize) } as *const u128;
-            unsafe { ptr.read_unaligned() }
+            let o = offset as usize;
+            u128::from_le_bytes(self.bios.bytes[o..o + 16].try_into().unwrap())
         } else {
             self.io_read128(pa)
         }
@@ -217,8 +217,7 @@ impl Bus {
 
     pub fn ranged_write8(&mut self, va: u32, val: u8) {
         if let Some(offset) = map::SCRATCHPAD.contains(va) {
-            let ptr = unsafe { self.scratchpad.as_mut_ptr().add(offset as usize) } as *mut u8;
-            unsafe { ptr.write_unaligned(val) }
+            self.scratchpad[offset as usize] = val;
             return;
         }
 
@@ -232,11 +231,9 @@ impl Bus {
         };
 
         if let Some(offset) = map::RAM.contains(pa) {
-            let ptr = unsafe { self.ram.as_mut_ptr().add(offset as usize) } as *mut u8;
-            unsafe { ptr.write_unaligned(val) }
+            self.ram[offset as usize] = val;
         } else if let Some(offset) = map::SCRATCHPAD.contains(pa) {
-            let ptr = unsafe { self.scratchpad.as_mut_ptr().add(offset as usize) } as *mut u8;
-            unsafe { ptr.write_unaligned(val) }
+            self.scratchpad[offset as usize] = val;
         } else {
             self.io_write8(pa, val)
         }
@@ -244,26 +241,25 @@ impl Bus {
 
     pub fn ranged_write16(&mut self, va: u32, val: u16) {
         if let Some(offset) = map::SCRATCHPAD.contains(va) {
-            let ptr = unsafe { self.scratchpad.as_mut_ptr().add(offset as usize) } as *mut u16;
-            unsafe { ptr.write_unaligned(val) }
+            let o = offset as usize;
+            self.scratchpad[o..o + 2].copy_from_slice(&val.to_le_bytes());
             return;
         }
 
-        let cop0_asid = { self.read_cop0_asid() };
+        let cop0_asid = self.read_cop0_asid();
         let operating_mode = self.operating_mode;
 
-        let pa = {
-            let tlb = &mut self.tlb;
-            tlb.translate_address(va, AccessType::WriteHalfword, operating_mode, cop0_asid)
-                .unwrap_or_else(|_e| va)
-        };
+        let pa = self
+            .tlb
+            .translate_address(va, AccessType::WriteHalfword, operating_mode, cop0_asid)
+            .unwrap_or(va);
 
         if let Some(offset) = map::RAM.contains(pa) {
-            let ptr = unsafe { self.ram.as_mut_ptr().add(offset as usize) } as *mut u16;
-            unsafe { ptr.write_unaligned(val) }
+            let o = offset as usize;
+            self.ram[o..o + 2].copy_from_slice(&val.to_le_bytes());
         } else if let Some(offset) = map::SCRATCHPAD.contains(pa) {
-            let ptr = unsafe { self.scratchpad.as_mut_ptr().add(offset as usize) } as *mut u16;
-            unsafe { ptr.write_unaligned(val) }
+            let o = offset as usize;
+            self.scratchpad[o..o + 2].copy_from_slice(&val.to_le_bytes());
         } else {
             self.io_write16(pa, val)
         }
@@ -271,8 +267,8 @@ impl Bus {
 
     pub fn ranged_write32(&mut self, va: u32, val: u32) {
         if let Some(offset) = map::SCRATCHPAD.contains(va) {
-            let ptr = unsafe { self.scratchpad.as_mut_ptr().add(offset as usize) } as *mut u32;
-            unsafe { ptr.write_unaligned(val) }
+            let o = offset as usize;
+            self.scratchpad[o..o + 4].copy_from_slice(&val.to_le_bytes());
             return;
         }
 
@@ -286,11 +282,11 @@ impl Bus {
         };
 
         if let Some(offset) = map::RAM.contains(pa) {
-            let ptr = unsafe { self.ram.as_mut_ptr().add(offset as usize) } as *mut u32;
-            unsafe { ptr.write_unaligned(val) }
+            let o = offset as usize;
+            self.ram[o..o + 4].copy_from_slice(&val.to_le_bytes());
         } else if let Some(offset) = map::SCRATCHPAD.contains(pa) {
-            let ptr = unsafe { self.scratchpad.as_mut_ptr().add(offset as usize) } as *mut u32;
-            unsafe { ptr.write_unaligned(val) }
+            let o = offset as usize;
+            self.scratchpad[o..o + 4].copy_from_slice(&val.to_le_bytes());
         } else {
             self.io_write32(pa, val)
         }
@@ -298,8 +294,8 @@ impl Bus {
 
     pub fn ranged_write64(&mut self, va: u32, val: u64) {
         if let Some(offset) = map::SCRATCHPAD.contains(va) {
-            let ptr = unsafe { self.scratchpad.as_mut_ptr().add(offset as usize) } as *mut u64;
-            unsafe { ptr.write_unaligned(val) }
+            let o = offset as usize;
+            self.scratchpad[o..o + 8].copy_from_slice(&val.to_le_bytes());
             return;
         }
 
@@ -313,25 +309,27 @@ impl Bus {
         };
 
         if let Some(offset) = map::RAM.contains(pa) {
-            let ptr = unsafe { self.ram.as_mut_ptr().add(offset as usize) as *mut u64 };
-            unsafe { ptr.write_unaligned(val) }
+            let o = offset as usize;
+            self.ram[o..o + 8].copy_from_slice(&val.to_le_bytes());
         } else if let Some(offset) = map::SCRATCHPAD.contains(pa) {
-            let ptr = unsafe { self.scratchpad.as_mut_ptr().add(offset as usize) as *mut u64 };
-            unsafe { ptr.write_unaligned(val) }
+            let o = offset as usize;
+            self.scratchpad[o..o + 8].copy_from_slice(&val.to_le_bytes());
         } else if let Some(offset) = map::VU0.contains(pa) {
-            let ptr = if pa < 0x1100_4000 {
-                unsafe { self.vu0_data.as_mut_ptr().add(offset as usize) as *mut u64 }
+            let o = offset as usize;
+            if pa < 0x1100_4000 {
+                self.vu0_data[o..o + 8].copy_from_slice(&val.to_le_bytes());
             } else {
-                unsafe { self.vu0_code.as_mut_ptr().add((offset - 0x4000) as usize) as *mut u64 }
+                let co = (offset - 0x4000) as usize;
+                self.vu0_code[co..co + 8].copy_from_slice(&val.to_le_bytes());
             };
-            unsafe { ptr.write_unaligned(val) }
         } else if let Some(offset) = map::VU1.contains(pa) {
-            let ptr = if pa < 0x1100_C000 {
-                unsafe { self.vu1_data.as_mut_ptr().add(offset as usize) as *mut u64 }
+            let o = offset as usize;
+            if pa < 0x1100_C000 {
+                self.vu1_data[o..o + 8].copy_from_slice(&val.to_le_bytes());
             } else {
-                unsafe { self.vu1_code.as_mut_ptr().add((offset - 0x4000) as usize) as *mut u64 }
+                let co = (offset - 0x4000) as usize;
+                self.vu1_code[co..co + 8].copy_from_slice(&val.to_le_bytes());
             };
-            unsafe { ptr.write_unaligned(val) }
         } else {
             self.io_write64(pa, val)
         }
@@ -339,39 +337,41 @@ impl Bus {
 
     pub fn ranged_write128(&mut self, va: u32, val: u128) {
         if let Some(offset) = map::SCRATCHPAD.contains(va) {
-            let ptr = unsafe { self.scratchpad.as_mut_ptr().add(offset as usize) } as *mut u128;
-            unsafe { ptr.write_unaligned(val) }
+            let o = offset as usize;
+            self.scratchpad[o..o + 16].copy_from_slice(&val.to_le_bytes());
             return;
         }
-        let cop0_asid = { self.read_cop0_asid() };
+
+        let cop0_asid = self.read_cop0_asid();
         let operating_mode = self.operating_mode;
 
-        let pa = {
-            let tlb = &mut self.tlb;
-            tlb.translate_address(va, AccessType::WriteDoubleword, operating_mode, cop0_asid)
-                .unwrap_or_else(|_e| va)
-        };
+        let pa = self
+            .tlb
+            .translate_address(va, AccessType::WriteDoubleword, operating_mode, cop0_asid)
+            .unwrap_or(va);
 
         if let Some(offset) = map::RAM.contains(pa) {
-            let ptr = unsafe { self.ram.as_mut_ptr().add(offset as usize) } as *mut u128;
-            unsafe { ptr.write_unaligned(val) }
+            let o = offset as usize;
+            self.ram[o..o + 16].copy_from_slice(&val.to_le_bytes());
         } else if let Some(offset) = map::SCRATCHPAD.contains(pa) {
-            let ptr = unsafe { self.scratchpad.as_mut_ptr().add(offset as usize) } as *mut u128;
-            unsafe { ptr.write_unaligned(val) }
+            let o = offset as usize;
+            self.scratchpad[o..o + 16].copy_from_slice(&val.to_le_bytes());
         } else if let Some(offset) = map::VU0.contains(pa) {
-            let ptr = if pa < 0x1100_4000 {
-                unsafe { self.vu0_data.as_mut_ptr().add(offset as usize) as *mut u128 }
+            let o = offset as usize;
+            if pa < 0x1100_4000 {
+                self.vu0_data[o..o + 16].copy_from_slice(&val.to_le_bytes());
             } else {
-                unsafe { self.vu0_code.as_mut_ptr().add((offset - 0x4000) as usize) as *mut u128 }
+                let co = (offset - 0x4000) as usize;
+                self.vu0_code[co..co + 16].copy_from_slice(&val.to_le_bytes());
             };
-            unsafe { ptr.write_unaligned(val) }
         } else if let Some(offset) = map::VU1.contains(pa) {
-            let ptr = if pa < 0x1100_C000 {
-                unsafe { self.vu1_data.as_mut_ptr().add(offset as usize) as *mut u128 }
+            let o = offset as usize;
+            if pa < 0x1100_C000 {
+                self.vu1_data[o..o + 16].copy_from_slice(&val.to_le_bytes());
             } else {
-                unsafe { self.vu1_code.as_mut_ptr().add((offset - 0x4000) as usize) as *mut u128 }
+                let co = (offset - 0x4000) as usize;
+                self.vu1_code[co..co + 16].copy_from_slice(&val.to_le_bytes());
             };
-            unsafe { ptr.write_unaligned(val) }
         } else {
             self.io_write128(pa, val)
         }
